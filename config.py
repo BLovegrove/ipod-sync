4ae8e1c52ed806386e562607dd07c6ce5aaf4e3a
Brandy Lovegrove <brandy@lovegrove.co.nz>
2024-01-14 17:30:11 +1300

    first commit

---
+++ config.py
@@ -0,0 +1,4 @@
+remote_library = "/home/brandy/plexlib_shared/music_smol"
+local_library = "./isync/library"
+lockfile = "library.lock"
+lock_warning = "Locked! Another part of your script is running on the library."
